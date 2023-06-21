4f547354efbb2431305c726f3f4170350e3f5ec5
Maksim Gulay <maksimgulaj@gmail.com>
2023-06-22 00:45:27 +0300

    Task 3 - feedback form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash/throttle'
+
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const form = document.querySelector('.feedback-form');
+
+const saveFormState = throttle(() => {
+    const formState = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+  }, 500);
+
+const restoreFormState = () => {
+  const savedState = localStorage.getItem('feedback-form-state');
+  if (savedState) {
+    const formState = JSON.parse(savedState);
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  }
+};
+
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+
+  localStorage.removeItem('feedback-form-state');
+
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formState);
+
+  emailInput.value = '';
+  messageInput.value = '';
+
+};
+
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
+
+form.addEventListener('submit', handleFormSubmit);
+
+window.addEventListener('load', restoreFormState);
 
