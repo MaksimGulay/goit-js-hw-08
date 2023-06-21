1fa6f9c1d52dd4c021dd5411266b99be371fdfdf
Maksim Gulay <maksimgulaj@gmail.com>
2023-06-21 22:35:03 +0300

    Task 2 - video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle'
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000));
+
+iframe.addEventListener('load', function() {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    if (savedTime) {
+        const seekTime = parseFloat(savedTime);
+        player.setCurrentTime(seekTime)
+            .then(function(seconds) {
+                // seconds = the actual time that the player seeked to
+            })
+            .catch(function(error) {
+                switch (error.name) {
+                    case 'RangeError':
+                                    // the time was less than 0 or greater than the video’s duration
+                        break;
+                    default:
+                        // some other error occurred
+                        break;
+                }
+            });
+    }
+});
+
