0a84fc94202ea73ffee8ee4d606a5c4f3fe2edb5
Maksim Gulay <maksimgulaj@gmail.com>
2023-06-21 16:41:28 +0300

    add galary task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+const gallaryList = document.querySelector('.gallery');
+
+const markup = galleryItems
+.map((item) => `
+<li class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+<img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+</li>`).join("");
+gallaryList.innerHTML = markup;
+
+gallaryList.style.listStyle = 'none'
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: "alt",
+    spinner: true,
+});
 
 console.log(galleryItems);
